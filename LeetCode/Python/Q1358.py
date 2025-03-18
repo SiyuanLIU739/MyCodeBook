d17f868d301d5addfa9426e5d24747606b366e29
Siyuan LIU <sliu211@seas.upenn.edu>
2025-03-17 23:56:10 -0400

    q1358

---
+++ LeetCode/Python/Q1358.py
@@ -0,0 +1,44 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        s = " " + s
+        fa = [0]
+        fb = [0]
+        fc = [0]
+
+        for i in range(1, len(s)):
+            fa.append(fa[-1])
+            fb.append(fb[-1])
+            fc.append(fc[-1])
+
+            if(s[i] == 'a'):
+                fa[-1] += 1
+            if(s[i] == 'b'):
+                fb[-1] += 1
+            if(s[i] == 'c'):
+                fc[-1] += 1
+
+        ans = 0
+        for i in range(1, len(s)):
+            if(fa[i] == 0 or fb[i] == 0 or fc[i] == 0):
+                continue
+            
+            index = len(s) * 2
+            index = min(index, self.find_index(fa, 0, i, fa[i] - 1))
+            index = min(index, self.find_index(fb, 0, i, fb[i] - 1))
+            index = min(index, self.find_index(fc, 0, i, fc[i] - 1))
+
+        print(self.find_index(fb, 0, 3, 1))
+
+    def find_index(self, f, l, r, target):
+        result = -1
+        while(l <= r):
+            mid = (l + r) // 2
+            if(f[mid] == target):
+                result = mid
+
+            if(f[mid] <= target):
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return result

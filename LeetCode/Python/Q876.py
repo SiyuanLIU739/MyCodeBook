d512b07ad789617535f425a4aeaf53e954baf721
Siyuan LIU <sliu211@seas.upenn.edu>
2025-09-17 21:06:40 -0400

    Q876 new sol

--- LeetCode/Python/Q876.py
+++ LeetCode/Python/Q876.py
@@ -21,3 +21,25 @@ class Solution:
     
     def midCnt(self, cnt):
         return cnt // 2 + 1
+    
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        i = 0
+        mid = head
+
+        while (head is not None):
+            i += 1
+            if(i % 2 == 0):
+                mid = mid.next
+            head = head.next
+
+        return mid

8e2e08cffead357812bec6274086794ae01a85cb
Siyuan LIU <sliu211@seas.upenn.edu>
2025-11-18 19:10:09 -0500

    224 rephrase

--- LeetCode/Python/Q224.py
+++ LeetCode/Python/Q224.py
@@ -1,5 +1,5 @@
 class Solution:
-    def calculate(self, s: str) -> int:
+    def calculate2(self, s: str) -> int:
         if(s[0] != '('):
             s = "0" + s
         leftp = []
@@ -68,6 +68,45 @@ class Solution:
             i += 2
 
         return ans
+    
+    def calculate(self, s: str) -> int:
+        # filter out space
+        sprime = ""
+        for char in s:
+            if(char != ' '):
+                sprime += char
+        if(sprime[0] == '-'):
+            sprime = '0' + sprime
+        s = sprime
+        sprime = ""
+
+        leftp = []
+
+        for char in s:
+            if(char == ')'):
+                cal = self.basic_calculate(sprime[leftp[-1] + 1: ])
+                sprime = sprime[: leftp[-1]]
+                leftp.pop()
+
+                if(len(sprime) == 0 or sprime[-1] == '('):
+                    sprime = sprime + str(cal)
+                elif(sprime[-1] == '+'):
+                    if(cal < 0):
+                        sprime = sprime[: -1] + str(cal)
+                    else:
+                        sprime = sprime + str(cal)
+                elif(sprime[-1] == '-'):
+                    if(cal < 0):
+                        sprime = sprime[: -1] + '+' + str(-cal)
+                    else:
+                        sprime = sprime + str(cal)
+            else:
+                sprime += char
+                if(char == '('):
+                    leftp.append(len(sprime) - 1)
+
+        return self.basic_calculate(sprime)
+
     
 sol = Solution()
 print(sol.calculate("((-(2 + 3)))"))

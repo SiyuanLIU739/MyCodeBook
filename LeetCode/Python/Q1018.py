8962bd5c50770a8acc7905a715c8c3020f0c11ba
Siyuan LIU <sliu211@seas.upenn.edu>
2025-11-23 21:08:14 -0500

    1018

--- LeetCode/Python/Q1018.py
+++ LeetCode/Python/Q1018.py
@@ -1,3 +1,10 @@
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
-        
+        ans = []
+        value = 0
+
+        for num in nums:
+            value = value * 2 + num
+            ans.append(value % 5 == 0)
+
+        return ans

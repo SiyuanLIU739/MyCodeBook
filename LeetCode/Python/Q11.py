7746bb2ae254dfeb638b1ec6dcacae062ca50a51
Siyuan LIU <swanlau739@gmail.com>
2025-12-05 17:06:54 +0800

    11 upd method

--- LeetCode/Python/Q11.py
+++ LeetCode/Python/Q11.py
@@ -26,3 +26,24 @@ class Solution:
                 maxi = bars[i].index
         
         return ans
+    
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        positions = {}
+        for i in range(len(height)):
+            if(height[i] not in positions.keys()):
+                positions[height[i]] = []
+            positions[height[i]].append(i)
+
+        heights = sorted(positions.keys(), reverse = True)
+
+        maxi = -1
+        mini = len(height)
+        ans = 0
+
+        for h in heights:
+            maxi = max(maxi, positions[h][-1])
+            mini = min(mini, positions[h][0])
+            ans = max(ans, (maxi - mini) * min(height[maxi], height[mini]))
+
+        return ans

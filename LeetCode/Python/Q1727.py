4a37a3400304d137ddcd069d047e72563fd1e23f
Siyuan LIU <sliu211@seas.upenn.edu>
2026-03-17 12:18:12 -0400

    Q1727

--- LeetCode/Python/Q1727.py
+++ LeetCode/Python/Q1727.py
@@ -3,12 +3,25 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
 
-        f = [0] * m
-        h = [0] * n
+        ans = 0
 
+        lastHeights = []
         for i in range(m):
+            seen = set()
+            heights = []
+            for lastHeight in lastHeights:
+                if(matrix[i][lastHeight[1]] == 1):
+                    heights.append((lastHeight[0] + 1, lastHeight[1]))
+                    seen.add(lastHeight[1])
+            
             for j in range(n):
-                f[i] += matrix[i][j]
-                h[j] += matrix[i][j]
+                if(j in seen): 
+                    continue
+                if(matrix[i][j] == 1):
+                    heights.append((1, j))
 
-        
+            for j in range(len(heights)):
+                ans = max(ans, heights[j][0] * (j + 1))
+            lastHeights = heights
+
+        return ans

f1c3c15d7c28d047e2e030678e773157e202cf31
Siyuan LIU <sliu211@seas.upenn.edu>
2026-05-03 18:09:47 -0400

    Q448 upd

--- LeetCode/Python/Q448.py
+++ LeetCode/Python/Q448.py
@@ -9,3 +9,18 @@ class Solution:
                 ans.append(i + 1)
 
         return ans
+    
+
+# upd
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            if(nums[abs(nums[i]) - 1] > 0):
+                nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
+
+        ans = []
+        for i in range(len(nums)):
+            if(nums[i] > 0):
+                ans.append(i + 1)
+        
+        return ans

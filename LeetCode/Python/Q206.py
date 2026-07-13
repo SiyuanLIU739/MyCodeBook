00f325c818112ffa719888770ad296e36f1559f3
Siyuan LIU <sliu211@seas.upenn.edu>
2026-07-13 19:21:10 -0400

    Q206 upd

--- LeetCode/Python/Q206.py
+++ LeetCode/Python/Q206.py
@@ -21,3 +21,26 @@ class Solution:
             nodes[i].next = nodes[i - 1]
 
         return nodes[-1]
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if(head is None):
+            return None
+
+        a = head
+        if(a.next is None):
+            return a
+        
+        b = a.next
+        a.next = None
+        if(b.next is None):
+            b.next = a
+            return b
+        
+        c = b.next
+        while(c is not None):
+            c = b.next
+            b.next = a
+            a = b
+            b = c
+
+        return a

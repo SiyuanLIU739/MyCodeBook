0c838692c6521b44be61e6790b1d3f4095f362d7
Siyuan LIU <sliu211@seas.upenn.edu>
2026-07-20 18:47:08 -0400

    Q882

---
+++ LeetCode/Python/Q882.py
@@ -0,0 +1,51 @@
+import heapq
+
+class Solution:
+    def reachableNodes(self, edges: List[List[int]], maxMoves: int, n: int) -> int:
+        graph = [[None] * n for _ in range(n)]
+        utilization = [[0] * n for _ in range(n)]
+
+        for edge in edges:
+            fr = edge[0]
+            to = edge[1]
+            subs = edge[2]
+
+            graph[fr][to] = subs
+            graph[to][fr] = subs
+
+        visited = [False] * n
+
+        q = [(0, 0)]
+        while(q):
+            move, fr = heapq.heappop(q)
+
+            if(visited[fr]):
+                continue
+            visited[fr] = True
+            
+            for to in range(n):
+                if(graph[fr][to] is None):
+                    continue
+                
+                if(utilization[to][fr] != 0 and utilization[to][fr] == graph[fr][to]):
+                    continue
+
+                if(move + graph[fr][to] + 1 <= maxMoves):
+                    heapq.heappush(q, (move + graph[fr][to] + 1, to))
+                    utilization[fr][to] = graph[fr][to]
+                
+                else:
+                    utilization[fr][to] = maxMoves - move
+
+
+        ans = 0
+        for fr in range(n):
+            if(visited[fr]):
+                ans += 1
+            for to in range(fr + 1, n):
+                ans += min(graph[fr][to] if graph[fr][to] is not None else 0, utilization[fr][to] + utilization[to][fr])
+
+        return ans
+
+
+            
